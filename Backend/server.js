8f94fcd17c91e5d4bb0ee13b0eb71f66f9fe3795
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2024-11-07 11:27:20 +0600

    set up database connection with mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
+const port = 5000;
 
-app.listen(process.env.PORT,()=> {
-    console.log(`Server is running on port ${process.env.PORT}`);
+
+app.listen(port,()=> {
+    console.log(`Server is running on port ${port}`);
 });

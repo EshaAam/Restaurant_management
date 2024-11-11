4c494cbb4660615260d06022b5b30faa564ad18d
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2024-11-11 22:21:24 +0600

    uploaded images

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                Heyy
+            </div>
+            <div className={show?"navLinks showmenu" : "navLinks"}>
+                
+            </div>
+        </nav>
+    );
+};
